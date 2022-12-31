7978f46f857bd7dbbce1e1b1ec9a9725ec25f61f
dhruvk-081 <115366526+dhruvk-081@users.noreply.github.com>
2022-12-31 12:41:22 +0530

    Update map.js

--- map.js
+++ map.js
@@ -2,7 +2,16 @@ var latLong = "";
 var lat = "";
 var long = "";
 const map = document.getElementById("map");
-$.getJSON(`https://ipinfo.io/103.57.84.135/json?token=894c517946f314`, function (data) {
+var ip_Address = "";
+
+$.getJSON(`https://api.ipify.org/?format=json`, function (data) {
+  //   console.log(data);
+  ip_Address = data.ip;
+});
+
+
+setTimeout(() => {
+$.getJSON(`https://ipinfo.io/${ip_Address}/json?token=894c517946f314`, function (data) {
   latLong = data.loc;
   var latLongSplit = latLong.split(",");
   lat = latLongSplit[0];
@@ -14,3 +23,5 @@ let link = `https://maps.google.com/maps?q=${lat},${long} &output=embed`;
 const iframe = document.createElement("iframe");
 iframe.src = link;
 map.appendChild(iframe);
+
+}, 1000);
